15c0f96c4df00b9c4548ccf3b1c26a89df51f86f
Elias <eliasneme2@gmail.com>
2020-10-11 21:14:27 +0200

    started on baseline model

---
+++ src/models.py
@@ -0,0 +1,10 @@
+import pytorch_lightning as pl
+
+
+class resnet_baseline(pl.LightningModule):
+    def __init__(self):
+        pass
+
+# TODO: Finish baselines
+
+# TODO: create a datamodule for the lyft dataset
