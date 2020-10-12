1167b59a5a7d435128c6991b4e5bd7cfa1358861
Elias <elias.nehme1@cevt.se>
2020-10-12 17:03:21 +0200

    WIP: baseline model

--- src/models.py
+++ src/models.py
@@ -1,9 +1,59 @@
 import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+from torchvision.models.resnet import resnet50
+import torch.functional as F
 
 
 class resnet_baseline(pl.LightningModule):
-    def __init__(self):
-        pass
+    """
+    This is the resnet baseline model provided in
+    agent_motion_prediction.ipynb, but transformed
+    into a pytorch lightning module.
+    """
+
+    def __init__(self, lr, num_history_channels, cfg):
+        super(resnet_baseline, self).__init__()
+
+        self.lr = lr
+        self.cfg = cfg
+        # change input channels number to match the rasterizer's output
+        self.num_in_channels = 3 + num_history_channels
+
+        self.model = resnet50(pretrained=True)
+
+        self.model.conv1 = nn.Conv2d(
+            self.num_in_channels,
+            self.model.conv1.out_channels,
+            kernel_size=self.model.conv1.kernel_size,
+            stride=self.model.conv1.stride,
+            padding=self.model.conv1.padding,
+            bias=False,
+        )
+
+        # change output size to (X, Y) * number of future states
+        num_targets = 2 * self.cfg["model_params"]["future_num_frames"]
+        self.model.fc = nn.Linear(in_features=2048, out_features=num_targets)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters, lr=self.lr)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+
+        logits = self(x).reshape(y.shape)
+
+        loss = F.mse_loss(logits, y)
+
+        result = pl.TrainResult(loss)
+
+        result.log("train_loss", loss, prog_bar=True)
+
+        return result
+
 
 # TODO: Finish baselines
 
---
+++ src/train.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+from l5kit.configs import load_config_data
+from l5kit.data import ChunkedDataset, LocalDataManager
+from l5kit.dataset import AgentDataset, EgoDataset
+from l5kit.evaluation import (
+    compute_metrics_csv,
+    create_chopped_dataset,
+    read_gt_csv,
+    write_pred_csv,
+)
+from l5kit.evaluation.chop_dataset import MIN_FUTURE_STEPS
+from l5kit.evaluation.metrics import neg_multi_log_likelihood, time_displace
+from l5kit.geometry import transform_points
+from l5kit.rasterization import build_rasterizer
+from l5kit.visualization import (
+    PREDICTED_POINTS_COLOR,
+    TARGET_POINTS_COLOR,
+    draw_trajectory,
+)
+from prettytable import PrettyTable
+
+import models
+import os
+
+# set env variable for data
+os.environ["L5KIT_DATA_FOLDER"] = "data"
+dm = LocalDataManager(None)
+# get config
+cfg = load_config_data("src/train_config.yaml")
+
+
+lr = 0.0003
+num_history_channels = (cfg["model_params"]["history_num_frames"] + 1) * 2
+
+
+model = models.resnet_baseline(lr, num_history_channels, cfg)
