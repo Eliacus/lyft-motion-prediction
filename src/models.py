5bf82d54485b71bc7a8381acccdf02c8700e3f05
Elias <eliasneme2@gmail.com>
2020-10-19 19:43:37 +0200

    implemented multimodal model and improved training pipeline

--- src/models.py
+++ src/models.py
@@ -1,17 +1,33 @@
 import os
-
+from pathlib import Path
+from utils import pytorch_neg_multi_log_likelihood_batch
+import numpy as np
 import pytorch_lightning as pl
 import torch
-import torch.nn.functional as F
 import torch.nn as nn
+import torch.nn.functional as F
 from l5kit.configs import load_config_data
 from l5kit.data import ChunkedDataset, LocalDataManager
-from l5kit.dataset import AgentDataset
-
+from l5kit.dataset import AgentDataset, EgoDataset
+from l5kit.evaluation import (
+    compute_metrics_csv,
+    create_chopped_dataset,
+    read_gt_csv,
+    write_pred_csv,
+)
+from l5kit.evaluation.chop_dataset import MIN_FUTURE_STEPS
+from l5kit.evaluation.metrics import neg_multi_log_likelihood, time_displace
+from l5kit.geometry import transform_points
 from l5kit.rasterization import build_rasterizer
+from l5kit.visualization import (
+    PREDICTED_POINTS_COLOR,
+    TARGET_POINTS_COLOR,
+    draw_trajectory,
+)
+from prettytable import PrettyTable
+from pytorch_lightning.loggers import TensorBoardLogger
 from torch.utils.data import DataLoader
 from torchvision.models.resnet import resnet50
-from pytorch_lightning.loggers import TensorBoardLogger
 
 
 class resnet_baseline(pl.LightningModule):
@@ -21,13 +37,18 @@ class resnet_baseline(pl.LightningModule):
     into a pytorch lightning module.
     """
 
-    def __init__(self, lr, num_history_channels, cfg):
+    def __init__(self, cfg, lr, num_modes=3):
         super(resnet_baseline, self).__init__()
+        self.save_hyperparameters()
 
-        self.lr = lr
-        self.cfg = cfg
+        self.cfg = self.hparams.cfg  # type: ignore
+        self.lr = self.hparams.lr  # type: ignore
+        self.num_modes = self.hparams.num_modes  # type: ignore
+
         # change input channels number to match the rasterizer's output
+        num_history_channels = (self.cfg["model_params"]["history_num_frames"] + 1) * 2
         self.num_in_channels = 3 + num_history_channels
+        self.future_len = cfg["model_params"]["future_num_frames"]
 
         self.model = resnet50(pretrained=True)
 
@@ -40,12 +61,30 @@ class resnet_baseline(pl.LightningModule):
             bias=False,
         )
 
-        # change output size to (X, Y) * number of future states
+        # change output size to (X, Y) * number of future states * num modes
         num_targets = 2 * self.cfg["model_params"]["future_num_frames"]
-        self.model.fc = nn.Linear(in_features=2048, out_features=num_targets)
+        self.num_preds = num_targets * self.num_modes
 
-    def forward(self, x):
-        return self.model(x)
+        self.model.fc = nn.Sequential(
+            nn.Linear(
+                in_features=2048,
+                # num of modes * preds + confidence
+                out_features=self.num_preds + self.num_modes,
+            ),
+        )
+
+        # Loss function
+        self.criterion = pytorch_neg_multi_log_likelihood_batch
+
+    def forward(self, data):
+        out = self.model(data)
+        batch_size = data.shape[0]
+        pred, confidences = torch.split(out, self.num_preds, dim=1)
+        assert pred.shape == (batch_size, self.num_preds)
+        assert confidences.shape == (batch_size, self.num_modes)
+        pred = pred.view(batch_size, self.num_modes, self.future_len, 2)
+        confidences = torch.softmax(confidences, dim=1)
+        return pred, confidences
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.lr)
@@ -54,30 +93,40 @@ class resnet_baseline(pl.LightningModule):
         x = batch["image"]
         y = batch["target_positions"]
 
-        target_availabilities = batch["target_availabilities"].unsqueeze(-1)
+        target_availabilities = batch["target_availabilities"]
 
-        logits = self(x).reshape(y.shape)
+        pred, confidences = self(x)
 
-        loss = F.mse_loss(logits, y)
-        loss = loss * target_availabilities
-        loss = loss.mean()
+        loss = self.criterion(y, pred, confidences, target_availabilities)
 
         # self.logger.experiment.add_scalar("loss", loss, global_step=batch_idx)
-        self.logger.log_metrics({"loss": loss}, step=self.global_step)
+        self.logger.log_metrics({"train_loss": loss}, step=self.global_step)
 
         # Option 1:
-        # return loss
+        return loss
 
         # Option 2:
-        return {"loss": loss}
+        # return {"train_loss": loss}
 
         # Option 3:
         # return {'loss': loss, 'hiddens': hiddens, 'anything_else': ...}
 
+    def validation_step(self, batch, batch_idx):
+        x = batch["image"]
+        y = batch["target_positions"]
 
-# TODO: Finish baselines
+        target_availabilities = batch["target_availabilities"]
 
-# TODO: create a datamodule for the lyft dataset
+        pred, confidences = self(x)
+
+        loss = self.criterion(y, pred, confidences, target_availabilities)
+
+        # self.logger.experiment.add_scalar("loss", loss, global_step=batch_idx)
+        self.logger.log_metrics({"val_loss": loss}, step=self.global_step)
+
+        self.log("val_loss", loss)
+
+        return loss
 
 
 class LyftDataModule(pl.LightningDataModule):
@@ -103,8 +152,8 @@ class LyftDataModule(pl.LightningDataModule):
         test_zarr = ChunkedDataset(self.dm.require(self.test_cfg["key"])).open()
 
         self.train_dataset = AgentDataset(self.cfg, train_zarr, self.rasterizer)
-        self.train_dataset = AgentDataset(self.cfg, val_zarr, self.rasterizer)
-        self.train_dataset = AgentDataset(self.cfg, test_zarr, self.rasterizer)
+        self.val_dataset = AgentDataset(self.cfg, val_zarr, self.rasterizer)
+        self.test_dataset = AgentDataset(self.cfg, test_zarr, self.rasterizer)
 
     def train_dataloader(self):
         return DataLoader(
--- src/train.py
+++ src/train.py
@@ -4,33 +4,37 @@ from l5kit.configs import load_config_data
 from l5kit.data import LocalDataManager
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
-
+from pytorch_lightning.callbacks import ModelCheckpoint
 import models
 
+
+# Create the tensorboard logger
 logger = TensorBoardLogger("tb_logs", name="my_model")
 
-# set env variable for data
-os.environ["L5KIT_DATA_FOLDER"] = "data"
-dm = LocalDataManager(None)
-# get config
-cfg = load_config_data("src/train_config.yaml")
-
+# Create the validation loss checkpoint
+checkpoint_callback = ModelCheckpoint(monitor="val_loss", mode="min")
 
 lr = 0.003
-num_history_channels = (cfg["model_params"]["history_num_frames"] + 1) * 2
+num_modes = 3
 
-# data_path = "/home/elias/Documents/lyft-motion-prediction/data"
-# config_path = "/home/elias/Documents/lyft-motion-prediction/src/train_config.yaml"
+data_path = "/home/elias/Documents/lyft-motion-prediction/data"
+config_path = "/home/elias/Documents/lyft-motion-prediction/src/train_config.yaml"
 
-data_path = "/home/elias.nehme1/Documents/lyft-motion-prediction/data"
-config_path = (
-    "/home/elias.nehme1/Documents/lyft-motion-prediction/src/train_config.yaml"
-)
+# data_path = "/home/elias.nehme1/Documents/lyft-motion-prediction/data"
+# config_path = (
+#    "/home/elias.nehme1/Documents/lyft-motion-prediction/src/train_config.yaml"
+# )
 
 lyft_data = models.LyftDataModule(data_path, config_path)
 
-model = models.resnet_baseline(lr, num_history_channels, lyft_data.cfg)
+model = models.resnet_baseline(lyft_data.cfg, lr, num_modes)
 
-trainer = Trainer(gpus=1, logger=logger, precision=16)
+trainer = Trainer(
+    logger=logger,
+    checkpoint_callback=checkpoint_callback,
+    val_check_interval=0.1,
+    gpus=1,
+    precision=16,
+)
 
 trainer.fit(model, lyft_data)
